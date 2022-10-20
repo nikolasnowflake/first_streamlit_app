e04c7ce637222f9526961975208af637f8c4da26
nikolasnowflake <116312429+nikolasnowflake@users.noreply.github.com>
2022-10-20 23:37:42 +0200

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('zdrava vecera mojih roditelja')
+streamlit.header('Dorucak je posluzen')
+streamlit.text('stavka 1, je jako bitna')
+streamlit.text('stavka 2, je jako bitna')
+streamlit.text('stavka 3, je jako bitna')
+streamlit.text('stavka 4, je jako bitna')
+streamlit.text('stavka 5, je jako bitna')
