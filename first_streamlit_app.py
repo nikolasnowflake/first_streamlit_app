f5f58cb93704a0c1fa870c0d7be9d92a47118404
nikolasnowflake <116312429+nikolasnowflake@users.noreply.github.com>
2022-10-20 23:51:42 +0200

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -7,5 +7,9 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 import pandas
+
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("Odaberi neko voce",my_fruit_list.index)
+
 streamlit.dataframe(my_fruit_list)
