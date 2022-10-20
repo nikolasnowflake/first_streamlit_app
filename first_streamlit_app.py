62e3d54a567e512b231a463e71729297f98b786f
nikolasnowflake <116312429+nikolasnowflake@users.noreply.github.com>
2022-10-20 23:39:37 +0200

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit
 
 streamlit.title('zdrava vecera mojih roditelja')
-streamlit.header('Dorucak je posluzen')
-streamlit.text('stavka 1, je jako bitna')
-streamlit.text('stavka 2, je jako bitna')
-streamlit.text('stavka 3, je jako bitna')
-streamlit.text('stavka 4, je jako bitna')
-streamlit.text('stavka 5, je jako bitna')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
