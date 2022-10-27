4afcd9b3fe6b7f34890a6b6910be618f0a34dd20
nikolasnowflake <116312429+nikolasnowflake@users.noreply.github.com>
2022-10-27 23:23:01 +0200

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -18,9 +18,12 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 streamlit.text(fruityvice_response.json())
 
 # pretvara podatke is json u regular 
